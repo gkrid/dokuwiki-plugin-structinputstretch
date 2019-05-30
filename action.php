90b35c5a4280444c351cce7928dfe60facc3fa61
Szymon Olewniczak <solewniczak@rid.pl>
2019-05-30 12:36:11 +0200

    plugin-wiz

---
+++ action.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * DokuWiki Plugin structinputstretch (Action Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Szymon Olewniczak <it@rid.pl>
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) {
+    die();
+}
+
+class action_plugin_structinputstretch extends DokuWiki_Action_Plugin
+{
+
+    /**
+     * Registers a callback function for a given event
+     *
+     * @param Doku_Event_Handler $controller DokuWiki's event controller object
+     *
+     * @return void
+     */
+    public function register(Doku_Event_Handler $controller)
+    {
+        $controller->register_hook('DOKUWIKI_STARTED', 'FIXME', $this, 'handle_dokuwiki_started');
+   
+    }
+
+    /**
+     * [Custom event handler which performs action]
+     *
+     * Called for event:
+     *
+     * @param Doku_Event $event  event object by reference
+     * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
+     *                           handler was registered]
+     *
+     * @return void
+     */
+    public function handle_dokuwiki_started(Doku_Event $event, $param)
+    {
+    }
+
+}
+
